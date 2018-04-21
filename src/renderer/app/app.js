8b8799e54b570c726d3a361049bb0643a99e95d5
Alessio Forte <fortealessio@gmail.com>
2018-04-21 18:37:31 +0200

    change App component

--- src/renderer/app/app.js
+++ src/renderer/app/app.js
@@ -8,36 +8,28 @@ import Suggestions from './suggestions'
 import Error from './error'
 import DidYouMean from './did-you-mean'
 
-class Resize extends Component {
+class App extends Component {
 
     componentDidUpdate() {
         setMainWindowSize()
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.translate !== this.props.translate || nextProps.error
-    }
-
     render() {
         return (
-            <div style={{ display: this.props.dropdown ? 'none' : 'block' }}>
-                <Textarea />
-                <Error />
-                <DidYouMean />
-                <Suggestions />
-                <Response />
-            </div>
+            <React.Fragment>
+                <LanguagesBar />
+                <div style={{ display: this.props.dropdown ? 'none' : 'block' }}>
+                    <Textarea />
+                    <Error />
+                    <DidYouMean />
+                    <Suggestions />
+                    <Response />
+                </div>
+            </React.Fragment>
         )
     }
 }
 
 const mapStateToProps = ({ dropdown, translate, error }) => ({ dropdown, translate, error })
 
-Resize = connect(mapStateToProps)(Resize)
-
-export default () => (
-    <React.Fragment>
-        <LanguagesBar />
-        <Resize />
-    </React.Fragment>
-)
+export default connect(mapStateToProps)(App)
