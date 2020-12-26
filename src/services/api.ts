9865b7a3a52835f471bf5889c16f1cfb0c53a925
Alessio Forte <alessioforte@mbpm1dialessio.homenet.telecomitalia.it>
2020-12-26 17:40:14 +0100

    add new google apis

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,124 +1,68 @@
-import querystring from 'querystring';
-import getToken from './token';
 import axios, { AxiosResponse } from 'axios';
 import { Langs } from './interfaces';
+import * as nodeRequest from 'request';
+import util from 'util';
+
+const request = util.promisify(nodeRequest);
 
 /**
  *
  * Google Translate APIs
  *
  */
+const googleUrl = 'https://translate.google.com/_/TranslateWebserverUi/data/batchexecute';
 
-export async function translate(
+export async function getGoogleTranslate(
   text: string,
   opts: Langs
-): Promise<AxiosResponse> {
-  const tk = await getToken(text);
-  const url = 'https://translate.google.com/translate_a/single';
-  const data = {
-    client: 'webapp',
-    sl: opts.from,
-    tl: opts.to,
-    hl: opts.to,
-    dt: [
-      'at',
-      'bd',
-      'ex',
-      'ld',
-      'md',
-      'qca',
-      'rw',
-      'rm',
-      'sos',
-      'ss',
-      't',
-      'gt',
-    ],
-    ie: 'UTF-8',
-    oe: 'UTF-8',
-    otf: 1,
-    ssel: 0,
-    tsel: 0,
-    kc: 7,
-    tk: tk.value,
-    q: text,
+  // ): Promise<AxiosResponse> {
+): Promise<any> {
+  console.log(text, opts);
+  const { from, to } = opts;
+  const options = {
+    method: 'POST',
+    url: googleUrl,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+    body: `f.req=%5B%5B%5B%22MkEWBc%22%2C%22%5B%5B%5C%22${encodeURI(
+      text
+    )}%5C%22%2C%5C%22${from}%5C%22%2C%5C%22${to}%5C%22%2Ctrue%5D%2C%5Bnull%5D%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&`,
   };
 
-  return await axios({
-    method: 'get',
-    url: url + '?' + querystring.stringify(data),
-  });
+  return await request(options);
 }
 
-export async function complete(
+export async function getGoogleSuggest(
   text: string,
-  lang: string
-): Promise<AxiosResponse> {
-  const url = 'https://clients1.google.com/complete/search';
-  const data = {
-    q: text,
-    client: 'translate-web',
-    ds: 'translate',
-    ie: 'UTF-8',
-    oe: 'UTF-8',
-    hl: lang,
-  };
-  return await axios({
-    method: 'get',
-    url: url + '?' + querystring.stringify(data),
-  });
-}
-
-export async function translateComplete(
-  words: string[],
   opts: Langs
-): Promise<AxiosResponse> {
-  const text = words.join('');
-  const tk = await getToken(text);
-  const url = 'https://translate.google.com/translate_a/t';
-  const data = {
-    client: 't',
-    sl: opts.from,
-    tl: opts.to,
-    hl: opts.to,
-    v: '1.0',
-    source: 'is',
-    ie: 'UTF-8',
-    oe: 'UTF-8',
-    tk: tk.value,
-    q: [...words],
+): Promise<any> {
+  const { from, to } = opts;
+  const options = {
+    method: 'POST',
+    url: googleUrl,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+    body: `f.req=%5B%5B%5B%22AVdN8%22%2C%22%5B%5C%22${text}%5C%22%2C%5C%22${from}%5C%22%2C%5C%22${to}%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&at=AD08yZlUAjeA4YyDVJDBzlMdrQXB%3A1608905462662&`,
   };
-
-  return await axios({
-    method: 'get',
-    url: url + '?' + querystring.stringify(data),
-  });
+  return request(options);
 }
 
-export async function voice(
+export async function getGoogleVoice(
   text: string,
   lang: string,
   speed: boolean
-): Promise<string> {
-  const textlen = text.length;
-  const tk = await getToken(text);
-  const url = 'https://translate.google.com/translate_tts';
-  const data = {
-    ie: 'UTF-8',
-    q: text,
-    tl: lang,
-    total: 1,
-    idx: 0,
-    textlen: textlen,
-    tk: tk.value,
-    client: 'webapp',
-    prev: 'input',
+): Promise<any> {
+  const options = {
+    method: 'POST',
+    url: googleUrl,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+    body: `f.req=%5B%5B%5B%22jQ1olc%22%2C%22%5B%5C%22${text}%5C%22%2C%5C%22${lang}%5C%22%2C${speed}%2C%5C%22null%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&at=AD08yZlUAjeA4YyDVJDBzlMdrQXB%3A1608905462662&`,
   };
-  if (speed) {
-    data['ttsspeed'] = 0.24;
-  }
-
-  return url + '?' + querystring.stringify(data);
+  return request(options);
 }
 
 /**
